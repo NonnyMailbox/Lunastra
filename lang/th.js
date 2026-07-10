e650365f9369d6116a032f210e87913341b65ede
NonnyMailbox <pypop2018@gmail.com>
2026-07-10 19:32:14 +0700

    ๊Update LUNASTRA website

--- lang/th.js
+++ lang/th.js
@@ -2,9 +2,9 @@ const th = {
     home: 'หน้าแรก',
     about: 'เกี่ยวกับ',
     news: 'ข่าว',
-    information: 'ข้อมูล⏷',
+    information: 'ข้อมูล ⏷',
     project: 'โครงงาน',
     shop: 'ร้านค้า🛍️',
     welcome: 'ยินดีต้อนรับสู่โลกแฟนตาซีของเรา',
-    explore: 'สำรวจ✦',
+    explore: 'สำรวจ ✦',
 }
